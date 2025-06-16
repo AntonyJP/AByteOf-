db8f868f7aa7e635eed16c631ac4a28544d03f25
ANTONY JOSTIN PEREZ PARRALES <antony.perez143@std.uni.edu.ni>
2025-06-16 15:10:24 -0600

    se completo todo el desarrollo de la parte de la splash screen, solo falta agregarle algunos detalles

--- SplashScreen.cs
+++ SplashScreen.cs
@@ -1,10 +1,28 @@
 namespace AByteOf熊猫
 {
-    public partial class Form1 : Form
+    public partial class SplashScreen : Form
     {
-        public Form1()
+        public SplashScreen()
         {
             InitializeComponent();
+            string rutaLogo = Path.Combine(Application.StartupPath, "Assets", "A byte of.png");
+            pcbLogo .Image = Image.FromFile(rutaLogo);
+            pgbLoading.Value = 0;
+            tmrLoading.Start();
+        }
+
+        private void tmrLoading_Tick(object sender, EventArgs e)
+        {
+            pgbLoading.Value += 1;
+            if (pgbLoading.Value >= pgbLoading.Maximum)
+            {
+                tmrLoading.Stop();
+                this.Hide();
+                Log_in loginForm = new Log_in();
+                loginForm.ShowDialog();
+                
+                
+            }
         }
     }
 }
