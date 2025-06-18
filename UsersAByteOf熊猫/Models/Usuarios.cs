a391c6ac9d1a218c95dca85450d454ed2a1e27e4
ANTONY JOSTIN PEREZ PARRALES <antony.perez143@std.uni.edu.ni>
2025-06-17 20:13:45 -0600

    implementacion de la api para ver si no da fallas el codigo

---
+++ UsersAByteOf熊猫/Models/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersAByteOf熊猫.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(75)]
+        public string Nombre { get; set; }
+        [Required, EmailAddress]
+        public string Correo { get; set; }
+        [Required, MinLength(6)]
+        public string Contrasena { get; set; }
+    }
+}
